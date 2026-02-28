2337458d02cc81117707296b7d717cd3f5826b43
Chad Norvell <chadnorvell@pm.me>
2026-02-27 21:06:57 -0800

    mmm

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,8 +5,20 @@ end
 set -x EDITOR nvim
 set -x GOPATH "$HOME/.pkg/go"
 set -x MAKEFLAGS "-j20"
-set -x npm_config_prefix "$HOME/.local"
+set -x npm_config_prefix "$HOME/.pkg/npm"
 
 source "$HOME/.cargo/env.fish"
 
-fish_add_path -Pm "$HOME/.local/bin"
+if test -d "$GOPATH/bin"
+    fish_add_path -Pm "$GOPATH/bin"
+end
+
+if test -d "$npm_config_prefix/bin"
+    fish_add_path -Pm "$npm_config_prefix/bin"
+end
+
+if test -d "$HOME/.local/bin"
+    fish_add_path -Pm "$HOME/.local/bin"
+end
+
+direnv hook fish | source
