35395a6979cd180b01d79618ed747080d1549858
Chad Norvell <chadnorvell@pm.me>
2026-02-28 23:34:58 -0800

    update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,18 +3,26 @@ if status is-interactive
 end
 
 set -x EDITOR nvim
-set -x GOPATH "$HOME/.pkg/go"
 set -x MAKEFLAGS "-j20"
-set -x npm_config_prefix "$HOME/.pkg/npm"
 
-source "$HOME/.cargo/env.fish"
+if test -e "$HOME/.cargo/env.fish"
+    source "$HOME/.cargo/env.fish"
+end
 
+set -x GOPATH "$HOME/.pkg/go"
 if test -d "$GOPATH/bin"
     fish_add_path -Pm "$GOPATH/bin"
 end
 
+set -x npm_config_prefix "$HOME/.pkg/npm"
 if test -d "$npm_config_prefix/bin"
     fish_add_path -Pm "$npm_config_prefix/bin"
+end
+
+set -x PIPX_HOME "$HOME/.pkg/pipx"
+set -x PIPX_BIN_DIR "$HOME/.pkg/pipx/bin"
+if test -d "$PIPX_BIN_DIR"
+    fish_add_path -Pm "$PIPX_BIN_DIR"
 end
 
 if test -d "$HOME/.local/bin"
