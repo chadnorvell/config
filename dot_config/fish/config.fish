f7503fc6744eb880fa0c5fc177802e75ab3cbb82
Chad Norvell <chadnorvell@pm.me>
2026-03-08 15:49:41 -0700

    Fish config mods

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -12,8 +12,27 @@ if not set -q IN_FISH_SUBSHELL
     set -x EDITOR nvim
     set -x MAKEFLAGS -j20
 
+    if test -e "$HOME/.pkg/brew/bin/brew"
+        "$HOME/.pkg/brew/bin/brew" shellenv | source
+    end
+
+    set -x npm_config_prefix "$HOME/.pkg/npm"
+    if test -d "$npm_config_prefix/bin"
+        fish_add_path -Pm "$npm_config_prefix/bin"
+    end
+
     if test -e "$HOME/.deno/env.fish"
         source "$HOME/.deno/env.fish"
+    end
+
+    if test -d "$HOME/.bun/bin"
+        fish_add_path -Pm "$HOME/.bun/bin"
+    end
+
+    set -x PIPX_HOME "$HOME/.pkg/pipx"
+    set -x PIPX_BIN_DIR "$HOME/.pkg/pipx/bin"
+    if test -d "$PIPX_BIN_DIR"
+        fish_add_path -Pm "$PIPX_BIN_DIR"
     end
 
     if test -e "$HOME/.cargo/env.fish"
@@ -23,17 +42,6 @@ if not set -q IN_FISH_SUBSHELL
     set -x GOPATH "$HOME/.pkg/go"
     if test -d "$GOPATH/bin"
         fish_add_path -Pm "$GOPATH/bin"
-    end
-
-    set -x npm_config_prefix "$HOME/.pkg/npm"
-    if test -d "$npm_config_prefix/bin"
-        fish_add_path -Pm "$npm_config_prefix/bin"
-    end
-
-    set -x PIPX_HOME "$HOME/.pkg/pipx"
-    set -x PIPX_BIN_DIR "$HOME/.pkg/pipx/bin"
-    if test -d "$PIPX_BIN_DIR"
-        fish_add_path -Pm "$PIPX_BIN_DIR"
     end
 
     if test -d "$HOME/.local/bin"
